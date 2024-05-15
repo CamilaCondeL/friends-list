fa2c484ca6e6f4ef0be1a6de927416ebeb24f81f
camila.conde.lorente <camila.conde.lorente@gmail.com>
2024-05-14 21:24:11 -0400

    refactored integration test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import {React, act} from 'react';
+import {React , act} from 'react';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import FriendList from './components/FriendsList';
@@ -8,8 +8,6 @@ import detailMockData from './detailsMockData.json';
 import listMockData from './mockData.json';
 
 jest.mock('./repo/fetchUtils');
-fetchFriends.mockResolvedValue(listMockData);
-fetchDetails.mockResolvedValue(detailMockData);
 
 describe('FriendDetailsFlow', () => {
   beforeAll(() => {
@@ -20,10 +18,17 @@ describe('FriendDetailsFlow', () => {
     jest.useRealTimers();
   });
 
-  test('navigates to friend details on link click and fetches data', async () => {
-    fetchDetails.mockResolvedValueOnce(detailMockData);
-    fetchFriends.mockResolvedValueOnce(listMockData);
+  beforeEach(() => {
+    fetchFriends.mockResolvedValue(listMockData);
+    fetchDetails.mockResolvedValue(detailMockData);
+  });
 
+  afterEach(() => {
+    jest.clearAllTimers();
+    jest.clearAllMocks();
+  });
+
+  test('navigates to friend details on link click and fetches data', async () => {
     await act(async () => {
       render(
         <MemoryRouter initialEntries={['/']}>
@@ -39,13 +44,11 @@ describe('FriendDetailsFlow', () => {
       jest.runOnlyPendingTimers();
     });
 
-    // Wait for the friend list to render
     await waitFor(() => {
       const friendElements = screen.getAllByTestId('friend-card');
       expect(friendElements.length).toBe(listMockData.length);
     });
 
-    // Simulate click on the first friend's details link
     const detailsLink = screen.getAllByRole('link', { name: /details/i })[0];
     await act(async () => {
       fireEvent.click(detailsLink);
@@ -55,14 +58,10 @@ describe('FriendDetailsFlow', () => {
       jest.runOnlyPendingTimers();
     });
 
-    // Wait for the loading spinner to disappear and the details to render
     await waitFor(() => {
       expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
     });
 
-    // Assert the friend details are rendered
-    const friendDetailsElements = screen.getAllByTestId('friend-card');
-    expect(friendDetailsElements.length).toBe(1);
-    expect(screen.getByText("Steph Walters")).toBeInTheDocument();
+    expect(screen.getByText(detailMockData.fullname)).toBeInTheDocument();
   });
 });
